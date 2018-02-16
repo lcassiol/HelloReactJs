badf01332f03403cb90401b1f68900ab12773c7b
Cassio Carvalho <lccassiol@gmail.com>
2018-02-15 22:50:36 -0200

    Create component Total to show total value :sparkles:

---
+++ src/Total.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Total extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <h3>Total Cash: </h3>
+      </div>
+    );
+  }
+
+}
+
+
+export default Total;
